1d051170223278acac74edb32fa59a879f94f3f1
Emna Ferchichi <68947429+EmnaFerchichi@users.noreply.github.com>
2024-12-27 13:14:45 +0100

    Update Error.cshtml.cs

--- my-books/Pages/Error.cshtml.cs
+++ my-books/Pages/Error.cshtml.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Threading.Tasks; 
 
 namespace my_books.Pages
 {
